f7a5e2ff2345cc0722c5bd6f9f634c92b40325f7
Nick <mr.upgrade3@protonmail.com>
2022-10-02 01:15:29 +0300

    Rebuild app, add main functionality.

--- script/script.js
+++ script/script.js
@@ -2,35 +2,68 @@
 
 (() => {
 
-    let numId = 0;
-    const dataStorage = [];
-
-    const todoInput = document.querySelector(".todo_input");
     const taskList = document.querySelector(".todo_tasks-wrapper");
-    const addNewTask = document.querySelector(".todo_submit");
-    const checkBox = document.querySelector(".check");
+    const formTodo = document.querySelector(".control");
+    const inputTask = document.querySelector(".todo_input");
 
-    addNewTask.addEventListener("click", event => {
+    const taskKeeper = [];
+    let taskIdCounter = -1;
+
+    const data = JSON.parse(localStorage.getItem("tasks"));
+
+    const updateHtml = (taskObj) => {
+        const newLi = document.createElement("li");
+        newLi.innerHTML = `<li id="${taskObj.id}" class="item-task">
+            <span>${taskObj.task}</span>
+            <button class="cancel-task">
+                <img src="assets/todo-cancel.png" alt="Cancel">
+            </button>
+        </li>`;
+        taskList.append(newLi);
+    }
+
+    const newTask = (info) => {
+        taskIdCounter += 1;
+        const taskObj = {
+            task: info,
+            id: taskIdCounter,
+        };
+        taskKeeper.push(taskObj);
+        localStorage.setItem("tasks", JSON.stringify(taskKeeper));
+        updateHtml(taskObj);
+    };
+
+    formTodo.addEventListener("submit", event => {
         event.preventDefault();
-        const getTask = todoInput.value.trim();
-        if(getTask.length !== 0) {
-            const html = `<label class="form-control">
-                <input class="remove check" type="checkbox" name="checkbox"/>
-                <span>${getTask}</span>
-            </label>`
-            taskList.innerHTML += html;
-            numId += 1;
-            const newTask = {
-                id: +`${numId}`,
-                task: `${getTask}`,
-            };
-            dataStorage.push(newTask);
-            localStorage.setItem("tasks", JSON.stringify(dataStorage));
+        const info = inputTask.value.trim();
+        if(info.length !== 0) {
+            newTask(info);
+            inputTask.value = "";
+            inputTask.focus();
         }
     });
 
+    if(data !== null) {
+        for (let item of data) {
+            updateHtml(item);
+        }
+    }
 
-
+    taskList.addEventListener("click", (event) => {
+        for (let el of event.composedPath()) {
+            if (el.matches && el.matches("button.cancel-task")) {
+                let uniqueId = +(el.parentNode.getAttribute("id"));
+                for (let itemId of data) {
+                    if(itemId.id === uniqueId) {
+                        let getIndex = data.indexOf(itemId);
+                        data.splice(getIndex, 1);
+                        localStorage.setItem("tasks", JSON.stringify(data));
+                    }
+                }
+                el.parentNode.remove();
+            }
+        }
+    });
 
 
 
